ce36027c0581c0d1b237c368cd19c3c467e8a353
adel <adelbouhafs621@gmail.com>
2024-12-12 11:19:04 +0100

    commit#6 _countof

--- mTP_premier_prog.cpp
+++ mTP_premier_prog.cpp
@@ -21,14 +21,22 @@ int main()
 	nbCara = printf("Le nombre de caracteres\n");
 
 	int jour, mois, annee;
-	scanf_s("%f %f %f", &jour, &mois, &annee);
 	printf("Date : %f / %f / %f \n", jour, mois, annee);
+	scanf_s("%f %f %f", &jour, &mois, &annee);
 	_getch();
 
 	char c;
 	do {
 		c = getchar();
 	} while (c != EOF && c != '\n');
+
+	char nomSaisi[10];
+	int ageSaisi, nbHex;
+	printf("Entrer votre nom: ");
+	scanf_s("%9s", nomSaisi, _countof(nomSaisi));
+	printf("Entrer votre age: ");
+	scanf_s("%d", &ageSaisi);
+	printf("Mr. %s a %d ans\n", nomSaisi, ageSaisi);
 
 	
 
