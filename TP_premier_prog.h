ef886cb5a5476206f789d558ba525d170a4bab82
adel <adelbouhafs621@gmail.com>
2024-12-12 10:58:52 +0100

    Ajoutez des fichiers projet.

---
+++ TP_premier_prog.h
@@ -0,0 +1,10 @@
+#pragma once
+// Nom du projet : TP_premier_prog
+// Nom de l'auteur : Adel
+// Date de création : 09/09/2024
+// Révision ou version : (facultatif pour les Tps)
+// Version du compilateur : (facultatif)
+// Résumé : TP sur les premiers programmes
+
+
+
---
+++ mTP_premier_prog.cpp
@@ -0,0 +1,25 @@
+// Nom du projet : TP_premier_prog
+// Nom de l'auteur : Adel
+// Date de création : 09/09/2024
+// Révision ou version : (facultatif pour les Tps)
+// Version du compilateur : (facultatif)
+// Résumé : TP sur les premiers programmes
+
+#include <stdio.h>
+#include <conio.h>
+#include "TP_premier_prog.h"
+
+int main()
+{
+	
+	printf("Premier programme\n");
+	printf("\nSection %s promotion %d\n", "CIEL", 2026);
+	printf("\nLe point de fusion du mercure est de %f \xF8 C soit environ %f \xF8 C \n", -38.842, -38.842);
+	printf("Total : %d \n ", printf("Premier programme\n"));
+	int nbCara;
+	nbCara = printf("Le nombre de caracteres\n");
+
+	
+	
+}
+
